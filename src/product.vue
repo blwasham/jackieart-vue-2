e42b79eb5aa75c9b7964379c08733066022b3d85
EC2 Default User <ec2-user@ip-172-31-44-21.ec2.internal>
2019-06-04 02:50:01 +0000

    just a sample wire up between a parent and child component

---
+++ src/product.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+<h1>Product Title {{ title }}</h1>   
+<h2>{{ childData }}</h2>
+<p>style test - should be blue3</p>        
+    </div>
+</template>
+
+
+<script>
+    export default {
+        props: ['title'],
+        data() {
+            return {
+                childData: 'Hello from the child'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    p {
+        color: blue;
+    }
+</style>
